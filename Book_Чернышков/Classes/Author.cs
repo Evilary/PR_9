9884da46c0298614612bb4d91308f6ab23a56da8
qwelord <secondofall22@rambler.ru>
2025-11-15 10:55:11 +0500

    Создание класса Author. Чернышков

---
+++ Book_Чернышков/Classes/Author.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Чернышков.Classes
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        public string FIO { get; set; }
+
+        public Author(int ID, string FIO) {
+            
+            this.Id = ID;
+            this.FIO = FIO;
+
+        }
+
+        public static List<Author> AllAuthors() {
+        
+            List<Author> allAuthors = new List<Author>();
+
+            allAuthors.Add(new Author(1, "Виктор Пелевин"));
+            allAuthors.Add(new Author(2, "Александра Маринина"));
+            allAuthors.Add(new Author(3, "Ольга Герр"));
+
+            return allAuthors;
+        }
+    }
+}
